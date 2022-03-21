9f581bd5025615a204e7f916013cc7ad154b10d1
Wesley Moizaque <wesley.moizaque@fretebras.com.br>
2022-03-21 13:36:10 -0300

    Add challenge solution

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sock-merchant
+This is my simple resolution of the HackerRank challenge.
+
+### How I solved this problem:
+I thought about assigning all socks on a map, and then I separated by each "value" that the quotient was even.
+
+![img.png](/doc/Screenshot_2.jpg)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+func sockMerchant(n int32, ar []int32) int32 {
+    // Write your code here
+    socks := make(map[int32]int)
+    var socksPair int32
+
+    for _, sockColor := range ar {
+        socks[sockColor]++
+    }    
+    
+    for _, value := range socks {
+        socksPair += int32(value / 2)
+    }
+
+    return socksPair
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var ar []int32
+
+    for i := 0; i < int(n); i++ {
+        arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+        checkError(err)
+        arItem := int32(arItemTemp)
+        ar = append(ar, arItem)
+    }
+
+    result := sockMerchant(n, ar)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
